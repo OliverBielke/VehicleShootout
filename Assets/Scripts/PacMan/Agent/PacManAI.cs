ad55b6c9b8dcd363646076bc0baab7a2a5a387ca
Oliver Bielke <obielke@kth.se>
2026-05-08 14:43:45 +0200

    Made A* debugger show dynamic obstacles

--- Assets/Scripts/PacMan/Agent/PacManAI.cs
+++ Assets/Scripts/PacMan/Agent/PacManAI.cs
@@ -132,6 +132,7 @@ namespace PacMan.Agent
         
         private VoronoiPartitioning _voronoiPartitioning;
         private Dictionary<Vector2Int, VoronoiCellData> _currentVoronoi;
+        
         private static readonly Dictionary<Team, List<Vector3>> ConsumedEnemyCapsulesByTeam = new();
         private static readonly Dictionary<int, FoodSpawnInfo> FoodSpawnInfoById = new();
 
