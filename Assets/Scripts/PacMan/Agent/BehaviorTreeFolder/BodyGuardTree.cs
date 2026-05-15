7fe63843ecf2f7a656ad1a4564383dd108a37e9f
Oliver Bielke <obielke@kth.se>
2026-05-15 15:43:44 +0200

    Made use of the body guard tree

--- Assets/Scripts/PacMan/Agent/BehaviorTreeFolder/BodyGuardTree.cs
+++ Assets/Scripts/PacMan/Agent/BehaviorTreeFolder/BodyGuardTree.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine;
 
 namespace PacMan.Agent.BehaviorTreeFolder
 {
@@ -34,7 +33,7 @@ namespace PacMan.Agent.BehaviorTreeFolder
         {
             BTNode<BodyGuardBlackboard> root =
                 new SelectorNode<BodyGuardBlackboard>(
-                    "Defender Selector",
+                    "Body Guard Selector",
                     new System.Collections.Generic.List<BTNode<BodyGuardBlackboard>>
                     {
                         new SequenceNode<BodyGuardBlackboard>(
@@ -48,13 +47,163 @@ namespace PacMan.Agent.BehaviorTreeFolder
                                 new ActionNode<BodyGuardBlackboard>(
                                     "MoveToLeader",
                                     bb => BTDecision.Running(
-                                        AgentMode.Attack,
+                                        AgentMode.Defend,
                                         "MoveToLeader",
                                         hasTarget: true,
                                         targetPosition: bb.teamLeaderPosition
                                     )
                                 )
                             }
+                        ),
+
+                        new SequenceNode<BodyGuardBlackboard>(
+                            "Grab Power Capsule Sequence",
+                            new System.Collections.Generic.List<BTNode<BodyGuardBlackboard>>
+                            {
+                                new ConditionNode<BodyGuardBlackboard>(
+                                    "shouldGrabPowerCapsule",
+                                    bb => bb.shouldGrabPowerCapsule
+                                ),
+                                new ActionNode<BodyGuardBlackboard>(
+                                    "GrabPowerCapsule",
+                                    bb => BTDecision.Running(
+                                        AgentMode.Attack,
+                                        "GrabPowerCapsule",
+                                        hasTarget: true,
+                                        targetPosition: bb.powerCapsuleTargetPosition
+                                    )
+                                )
+                            }
+                        ),
+
+                        new SequenceNode<BodyGuardBlackboard>(
+                            "Return Home Sequence",
+                            new System.Collections.Generic.List<BTNode<BodyGuardBlackboard>>
+                            {
+                                new ConditionNode<BodyGuardBlackboard>(
+                                    "shouldReturnHome",
+                                    bb => bb.shouldReturnHome
+                                ),
+                                new ActionNode<BodyGuardBlackboard>(
+                                    "ReturnHome",
+                                    bb => BTDecision.Running(
+                                        AgentMode.ReturnHome,
+                                        "ReturnHome",
+                                        hasTarget: true,
+                                        targetPosition: bb.homeTargetPosition
+                                    )
+                                )
+                            }
+                        ),
+
+                        new SequenceNode<BodyGuardBlackboard>(
+                            "Loot While Powered Sequence",
+                            new System.Collections.Generic.List<BTNode<BodyGuardBlackboard>>
+                            {
+                                new ConditionNode<BodyGuardBlackboard>(
+                                    "shouldLootWhilePowered",
+                                    bb => bb.shouldLootWhilePowered
+                                ),
+                                new ActionNode<BodyGuardBlackboard>(
+                                    "CollectEnemyPills",
+                                    bb => BTDecision.Running(
+                                        AgentMode.Attack,
+                                        "CollectEnemyPills",
+                                        hasTarget: true,
+                                        targetPosition: bb.enemyPillTargetPosition
+                                    )
+                                )
+                            }
+                        ),
+
+                        new SequenceNode<BodyGuardBlackboard>(
+                            "Intercept Intruder Sequence",
+                            new System.Collections.Generic.List<BTNode<BodyGuardBlackboard>>
+                            {
+                                new ConditionNode<BodyGuardBlackboard>(
+                                    "enemyPacmanIntruderSuspected",
+                                    bb => bb.enemyPacmanIntruderSuspected
+                                ),
+                                new ActionNode<BodyGuardBlackboard>(
+                                    "InterceptIntruder",
+                                    bb => BTDecision.Running(
+                                        AgentMode.Defend,
+                                        "InterceptIntruder",
+                                        hasTarget: true,
+                                        targetPosition: bb.suspectedIntruderPosition
+                                    )
+                                )
+                            }
+                        ),
+
+                        new SequenceNode<BodyGuardBlackboard>(
+                            "Block Crossing Sequence",
+                            new System.Collections.Generic.List<BTNode<BodyGuardBlackboard>>
+                            {
+                                new ConditionNode<BodyGuardBlackboard>(
+                                    "enemyLikelyCrossingMyLane",
+                                    bb => bb.enemyLikelyCrossingMyLane
+                                ),
+                                new ActionNode<BodyGuardBlackboard>(
+                                    "BlockCrossing",
+                                    bb => BTDecision.Running(
+                                        AgentMode.Defend,
+                                        "BlockCrossing",
+                                        hasTarget: true,
+                                        targetPosition: bb.predictedCrossingPoint
+                                    )
+                                )
+                            }
+                        ),
+
+                        new SequenceNode<BodyGuardBlackboard>(
+                            "Collect Safe Middle Pills Sequence",
+                            new System.Collections.Generic.List<BTNode<BodyGuardBlackboard>>
+                            {
+                                new ConditionNode<BodyGuardBlackboard>(
+                                    "safeMiddlePillsAvailable",
+                                    bb => bb.safeMiddlePillsAvailable
+                                ),
+                                new ActionNode<BodyGuardBlackboard>(
+                                    "CollectSafeMiddlePills",
+                                    bb => BTDecision.Running(
+                                        AgentMode.Defend,
+                                        "CollectSafeMiddlePills",
+                                        hasTarget: true,
+                                        targetPosition: bb.safeMiddlePillPosition
+                                    )
+                                )
+                            }
+                        ),
+
+                        new SequenceNode<BodyGuardBlackboard>(
+                            "Move To Formation Sequence",
+                            new System.Collections.Generic.List<BTNode<BodyGuardBlackboard>>
+                            {
+                                new ConditionNode<BodyGuardBlackboard>(
+                                    "outsideDefensiveZone",
+                                    bb => bb.outsideDefensiveZone
+                                ),
+                                new ActionNode<BodyGuardBlackboard>(
+                                    "MoveToFormation",
+                                    bb => BTDecision.Running(
+                                        AgentMode.Defend,
+                                        "MoveToFormation",
+                                        hasTarget: true,
+                                        targetPosition: bb.formationPoint
+                                    )
+                                )
+                            }
+                        ),
+
+                        new ActionNode<BodyGuardBlackboard>(
+                            "HoldDropZone",
+                            bb => BTDecision.Running(
+                                AgentMode.Defend,
+                                "HoldDropZone",
+                                hasTarget: true,
+                                targetPosition: bb.dropZonePoint
+                            )
                         )
                     }
                 );
--- Assets/Scripts/PacMan/Agent/PacManAI.cs
+++ Assets/Scripts/PacMan/Agent/PacManAI.cs
@@ -43,6 +43,7 @@ namespace PacMan.Agent
         private GameObject _currentFoodTarget;
         private BehaviorTree<DefenderBlackboard> _defenderTree;
         private BehaviorTree<AttackerBlackboard> _attackerTree;
+        private BehaviorTree<BodyGuardBlackboard> _bodyGuardTree;
         private BTDecision _lastDecision;
         private string _btReason = "-";
         private StaticRole _staticAssignedRole = StaticRole.None;
@@ -244,6 +245,7 @@ namespace PacMan.Agent
             // All of the calls below should also work in here. Report it as a bug if you find that some part of the observations is inaccessible during init.
             _hasGoal = false;
             _defenderTree = DefenderTreeFactory.Create();
+            _bodyGuardTree = BodyGuardTreeFactory.Create();
             _attackerTree = AttackerTreeFactory.Create();
             _middleAnalyzer = new MapMiddleAnalyzer(_obstacleMap);
             _middleInfo = _middleAnalyzer.Analyze();
@@ -365,6 +367,21 @@ namespace PacMan.Agent
         }
         private BTDecision EvaluateCurrentRoleTree()
         {
+            // Check if this agent is a body guard (has a leader)
+            PacManAgentManager leader = null;
+            bool isBodyGuard = TeamAssigner.Instance != null && TeamAssigner.Instance.TryGetLeader(_agent, out leader);
+            
+            if (isBodyGuard && leader != null)
+            {
+                // This agent is a body guard, use the body guard tree
+                BodyGuardBlackboard bb = BuildBodyGuardBlackboard();
+                _btReason = bb.debugReason;
+                using (DebugManager.BeginTimingScope("AI/Decision/BodyGuardTree"))
+                {
+                    return _bodyGuardTree.Evaluate(bb);
+                }
+            }
+
             switch (_assignedRole)
             {
                 case StaticRole.Defend:
@@ -807,9 +824,9 @@ namespace PacMan.Agent
             return Mathf.Abs(stableId) % interval;
         }
         
-        private DefenderBlackboard BuildBodyGuardBlackboard()
+        private BodyGuardBlackboard BuildBodyGuardBlackboard()
         {
-            DefenderBlackboard bb = new DefenderBlackboard();
+            BodyGuardBlackboard bb = new BodyGuardBlackboard();
 
             Vector3 myPos = transform.localPosition;
             UpdateVoronoiData();
